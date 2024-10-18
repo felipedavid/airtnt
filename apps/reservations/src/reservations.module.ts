261adfe13b524fc047a964b962c42da63825963d
Felipe David <felipedavid.huh@gmail.com>
2024-10-18 17:30:40 -0300

    adding better logging and validation

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -3,10 +3,16 @@ import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common/database/database.module';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { ReservationsRepository } from './reservations.repository';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}])],
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}]),
+    LoggerModule,
+  ],
   controllers: [ReservationsController],
-  providers: [ReservationsService],
+  providers: [ReservationsService, ReservationsRepository],
 })
 export class ReservationsModule {}
