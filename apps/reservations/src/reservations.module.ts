efbf3da19986044531b15ca6cc1bf368c5ee92a1
Felipe David <felipedavid.huh@gmail.com>
2024-10-20 00:27:01 -0300

    adding more jwt stuff

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -5,12 +5,21 @@ import { DatabaseModule } from '@app/common/database/database.module';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
 import { ReservationsRepository } from './reservations.repository';
 import { LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.string().required(),
+        MONGODB_URI: Joi.string().required(),
+      })
+    })
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
